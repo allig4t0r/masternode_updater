d95f9ad54337d897a6396e62f85b1d7d11326887
e3b3c0 <e3b3c0@yandex-team.ru>
2023-01-06 15:05:44 +0400

    some changes for non-reboot

--- masternode-updater.sh
+++ masternode-updater.sh
@@ -4,6 +4,8 @@ echo Huuymas
 
 echo Tuning .bashrc
 echo -e "alias k=kubectl\nalias ll=\"ls -la\"" >> /root/.bashrc
+echo Reload bash
+exec bash
 
 echo HUY
 
@@ -11,9 +13,11 @@ echo Updating the /etc/apt/sources.list
 echo -e "deb http://mirror.yandex.ru/debian sid main contrib non-free\ndeb-src http://mirror.yandex.ru/debian sid main contrib non-free" >> /etc/apt/sources.list
 
 echo Enable br_netfilter
+modprobe br_netfilter
 echo br_netfilter | tee -a /etc/modules
 
 echo Enable ip_forward
+echo '1' > /proc/sys/net/ipv4/ip_forward
 echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 
 echo Updating apt
@@ -32,8 +36,8 @@ apt update
 echo Upgrading apt
 apt upgrade -y
 
-echo Installing kubelet kubeadm kubectl
-apt-get install -y kubelet kubeadm kubectl
+echo Installing kubelet kubeadm kubectl containerd netcat
+apt-get install -y kubelet kubeadm kubectl containerd netcat
 echo Placing hold on these packages
 apt-mark hold kubelet kubeadm kubectl
 
